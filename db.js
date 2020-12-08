8756900085d9b95e168c40b0b4978a1760151c8e
Elle Rose <ellrose@pop-os.localdomain>
2020-12-08 14:25:34 -0500

    .env

--- db.js
+++ db.js
@@ -3,23 +3,12 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 const pool = new Pool({
-    user: 'postgres',
-    password: 'donJuan777',
-    database: 'todo',
-    host: 'localhost',
-    port: 5432
+    user: `${process.env.PGUSER}`,
+    password: `${process.env.PGPASSWORD}`,
+    database: `${process.env.PGDATABASE}`,
+    host: `${process.env.PGHOST}`,
+    port: `${process.env.PGPORT}`
 
-    // user: `${process.env.PGUSER}`,
-    // password: `${process.env.PGPASSWORD}`,
-    // database: `${process.env.PGDATABASE}`,
-    // host: `${process.env.PGHOST}`,
-    // port: `${process.env.PGPORT}`
-
-    // user: process.env.PGUSER,
-    // password: process.env.PGPASSWORD,
-    // database: process.env.PGDATABASE,
-    // host: process.env.PGHOST,
-    // port: process.env.PGPORT
 });
 
 module.exports = pool;
